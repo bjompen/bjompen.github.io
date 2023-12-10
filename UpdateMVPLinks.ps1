afb0df42c96aad78f2ec7aae7fdb36d2a22bf177
Emanuel Palm <emanuel+github@pipe.how>
2023-12-10 21:49:52 +0100

    Updated script to handle multiple links per line
    Keeps track of the changes for the line until all matched links are updated before changing the file.

--- UpdateMVPLinks.ps1
+++ UpdateMVPLinks.ps1
@@ -1,4 +1,6 @@
 $MVPId = '?wt.mc_id=DT-MVP-5005317'
+
+# List of sites to update links for
 $sites = @(
     'https://social.technet.microsoft.com/',
     'https://docs.microsoft.com/',
@@ -21,30 +23,40 @@ $sites = @(
     'https://learn.microsoft.com/'
 )
 
-$AllPosts = Get-ChildItem .\posts\*.md
+# Get all posts
+$AllPosts = Get-ChildItem .\content\*.md -Recurse
 
+# Regex pattern to find links in markdown files
 $pattern = '(?<=]\()[^)]*'
 
 foreach ($site in $sites) {
     $hits = $AllPosts | Select-String -Pattern $site -AllMatches
     foreach ($hit in $hits) {
+        # Skip if the line already contains the MVPId
         if ($hit.Line -notlike "*$MVPId*") {
             $originalLine = $hit.Line
+            $newLine = $originalLine
             $patMatches = [regex]::matches($hit.Line, $pattern).Value
             foreach ($patMatch in $patMatches) {
                 [URI]$replaceUrl = $patMatch
                 if ($replaceUrl.OriginalString -like "$site*") {
                     if (-not [string]::IsNullOrWhiteSpace($replaceUrl.Query)) {
+                        # If the url already contains a query string, append the MVPId
                         $newQuery = "$($replaceUrl.Query)$($MVPId.Replace('?','&'))"
                     }
                     else {
                         $newQuery = $MVPId
                     }
+                    # Build the new url and remove the language part
                     $newUrl = "$($replaceUrl.Scheme)://$($replaceUrl.Host)$($replaceUrl.AbsolutePath.Replace('en-us/','').Replace('en-gb/','').TrimEnd('/'))$newQuery$($replaceUrl.Fragment)"
-                    $newLine = $originalLine.Replace($replaceUrl.OriginalString, $newUrl)
-
-                    (Get-Content $hit.Path -Raw).replace($originalLine, $newLine) | Set-Content $hit.Path -NoNewline
+                    # Keep track of the changes until we have all the changes in case there are multiple links in the same line
+                    $newLine = $newLine.Replace($replaceUrl.OriginalString, $newUrl)
                 }
+            }
+
+            # Replace the line in the file
+            if ($newLine -ne $originalLine) {
+                (Get-Content $hit.Path -Raw).replace($originalLine, $newLine) | Set-Content $hit.Path -NoNewline
             }
         }
     }
